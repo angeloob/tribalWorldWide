dfb20f6658ae853e621566e4a300cfe65959a638
Angel Obed Olvera Borjas <angelolvera@live.com.mx>
2021-06-17 11:00:01 -0500

    creacion de proyecto

---
+++ tribalWorldWideProof/tribalWorldWideProof/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tribalWorldWideProof
+//
+//  Created by Angel Olvera on 17/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
